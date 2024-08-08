70d7e3b98f82bc0d1d0a94b8e6c2fe3bb7f991cc
sayed saud <89771866+sayedsaud@users.noreply.github.com>
2024-08-08 15:21:38 +0530

    'bootstrap-thumbnail'

---
+++ eproject/eproject/Scripts/eproject/eapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=30)
+    description=models.TextField(max_length=300)
+
+
+    class Meta:
+        db_table='category'
+
+    def __str__(self):
+        return self.category_name
+
+class Product(models.Model):
+    p_name=models.CharField(max_length=30)
+    p_price=models.IntegerField()
+    p_description=models.TextField(max_length=300)
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table='product'
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    Product=models.ForeignKey(Product,on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table='cart'                
